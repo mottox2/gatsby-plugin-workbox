6f5189c661e53e523da9ac2a21b70de90f62ebea
mottox2 <y.mottox2@gmail.com>
2018-05-17 23:54:18 +0900

    Add fallbacck component

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,5 +1,18 @@
 
 const workboxBuild = require('workbox-build')
+const path = require(`path`)
+const slash = require(`slash`)
+const _ = require(`lodash`)
+
+exports.createPages = ({ boundActionCreators }) => {
+  if (process.env.NODE_ENV === `production`) {
+    const { createPage } = boundActionCreators
+    createPage({
+      path: `/offline-plugin-app-shell-fallback/`,
+      component: slash(path.resolve(`${__dirname}/app-shell.js`)),
+    })
+  }
+}
 
 exports.onPostBuild = (args, pluginOptions) => {
   workboxBuild.generateSW({
@@ -8,6 +21,7 @@ exports.onPostBuild = (args, pluginOptions) => {
     globPatterns: [
       '**\/*.{html,json,js,css}',
     ],
+    navigateFallback: `/offline-plugin-app-shell-fallback/index.html`,
     swDest: 'public/sw.js',
   })
 }
